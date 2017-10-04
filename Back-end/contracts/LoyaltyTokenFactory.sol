8a3547466b8f748aeb2276a9586245d69b41f309
Ranjitha Raghavendra <ranjitha.rezee@gmail.com>
2017-10-04 13:09:57 +0530

    Update LoyaltyTokenFactory.sol

--- Back-end/contracts/LoyaltyTokenFactory.sol
+++ Back-end/contracts/LoyaltyTokenFactory.sol
@@ -6,6 +6,10 @@ contract LoyaltyTokenFactory {
 
     address[] public tokens;
     mapping(address => address[]) ownerMap;
+    
+    //Token Count
+         LoyaltyToken[] tokensArray;
+
 
     function initialiseRetail(uint256 _initialAmount, string _name, uint8 _decimals, string _symbol) returns (address) {
 
@@ -13,7 +17,10 @@ contract LoyaltyTokenFactory {
         newToken.transfer(msg.sender, _initialAmount);       //the factory will own the created tokens. You must transfer them.
 
         ownerMap[msg.sender].push(address(newToken));   // to keep track of who created what
-        tokens.push(address(newToken));                 // a basic list of all tokens
+        tokens.push(address(newToken));   
+        tokensArray.push(newToken);
+
+        // a basic list of all tokens
 
         return address(newToken);
     }
@@ -25,5 +32,20 @@ contract LoyaltyTokenFactory {
     function getTokensAddressByOwner(address owner) constant returns (address[]) {
         return ownerMap[owner];
     }
+    
+    //Added for getting Tokens
+    
+    function getNumberOfTokens() returns (uint) {
+    	return tokensArray.length;
+    }
+
+    /**
+    * Returns TokenName, Symbol and Amount
+    */
+    function getTokenAtIndex(uint index) returns(bytes32 , bytes32 , uint8 decimal)
+ 	 {
+ 	 	LoyaltyToken selToken = tokensArray[index];
+    	return (selToken.getName(), selToken.getSymbol() , selToken.getDecimals()) ;
+ 	 }
 
 }
