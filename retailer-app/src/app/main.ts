c976f52dc3267a3a991e8cce5113a0b90c822e86
Marc Wafik <marc.wafik@hotmail.com>
2017-10-10 15:10:29 +0200

    added production mode

--- retailer-app/src/app/main.ts
+++ retailer-app/src/app/main.ts
@@ -1,5 +1,7 @@
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
-
+import { enableProdMode } from '@angular/core';
 import { AppModule } from './app.module';
+import CONFIG from '../app.config';
 
+if (CONFIG.IS_PRODUCTION) { enableProdMode(); }
 platformBrowserDynamic().bootstrapModule(AppModule);
