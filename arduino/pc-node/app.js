d43b0e82fa2d7d867057165251c109b92999a679
Marc Wafik <marc.wafik@gmail.com>
2017-11-08 15:07:27 +0200

    arduino refactor

--- arduino/pc-node/app.js
+++ arduino/pc-node/app.js
@@ -1,53 +1,45 @@
 const app = require('express')();
 const http = require('http').Server(app);
 const io = require('socket.io')(http);
+const SerialPort = require("serialport");
 
-
-var SerialPort = require("serialport");
+var serialPort = new SerialPort("COM5");
 
 io.on('connection', socket => {
+	console.log('client connected');
 
+	var output = "";
 
-//-----------------serial port Start
-var port = new SerialPort("COM5");
-//console.log(port);
-var output =""
-var serial ="";
- port.on('open', function(){
-    console.log('Serial Port Opend');
-    port.setEncoding("utf8");
-    port.on('data', function (data) {
-    for(let i =0;i<data.length;i++){
-            if(data[i] == "%"){
-                if(output.length >1){
-                    console.log(output);
-                }
-                
-                output = "";
-            }else if(data[i] == "&"){
-                serial = output;
-                console.log("Serial : "+ serial );
-                
-io.emit('public-key:sent', { publicKey:serial }); // send the key to the tcp socket
-            }else{
-                output += data[i] ;
-            }
-        }
-        
-      });
-  });
-//---------------------serialport End
+	serialPort.on('open', () => {
+		console.log('Serial Port Opend');
+		serialPort.setEncoding("utf8");
+		serialPort.on('data', (data) => {
+			for (let i = 0; i < data.length; i++) {
+				if (data[i] == "%") {
+					if (output.length > 1) {
+						console.log(output);
+					}
 
+					output = "";
+				} else if (data[i] == "&") {
+					let serial = output;
+					console.log(`serial: ${serial}`);
 
+					io.emit('public-key:sent', { publicKey: serial }); // send the key to the tcp socket
+				} else {
+					output += data[i];
+				}
+			}
 
+		});
+	});
 
-  console.log('client connected');
-  socket.on('disconnect', () => {
-    console.log('client disconnected');
-  });
+	socket.on('disconnect', () => {
+		console.log('client disconnected');
+	});
 });
 
-const port = process.env.PORT || 4040;
-app.listen(port, () => {
-  console.log(`server is listening on port ${port}!`);
+let socketPort = process.env.PORT || 4040;
+app.listen(socketPort, () => {
+	console.log(`server is listening on port ${socketPort}!`);
 })
