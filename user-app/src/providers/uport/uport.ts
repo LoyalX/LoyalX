a82625a552bb866ebf8b3bfc35a21680dd8e8981
Marc Wafik <marc.wafik@hotmail.com>
2017-12-24 21:23:57 +0200

    checkin

--- user-app/src/providers/uport/uport.ts
+++ user-app/src/providers/uport/uport.ts
@@ -3,7 +3,7 @@ import { Http } from '@angular/http';
 import 'rxjs/add/operator/map';
 
 import { Storage } from '@ionic/storage';
-import { Connect } from 'uport-connect'
+import { Connect, SimpleSigner } from 'uport-connect'
 
 /*
   Generated class for the UportProvider provider.
@@ -14,7 +14,6 @@ import { Connect } from 'uport-connect'
 @Injectable()
 export class UportProvider {
 
-  uPort = new Connect('LoyalX');
 
   constructor(public http: Http, public storage: Storage) {
     console.log('Hello UportProvider Provider');
@@ -22,11 +21,31 @@ export class UportProvider {
 
   async requestCredentials() {
 
-    var cred = await this.uPort.requestCredentials({
-      requested: ['name', 'country']
-    });
+    const uport = new Connect('LoyalX', {
+      clientId: '2ofupnomVfaXEY8Ueyq43GULkoZjgqEDXCN',
+      network: 'rinkeby or ropsten or kovan',
+      signer: SimpleSigner('f3fe7e6b5ec60fbbdde4dc67126f77634e631d89f5faf3ef06d9c64a0364d6a6')
+    })
 
+    // Request credentials to login
+    var cred = await uport.requestCredentials();
     this.storage.set('uPort', cred);
+
+    /*{
+      requested: ['name', 'phone', 'country'],
+      notifications: true // We want this if we want to recieve credentials
+    })*/
+
+    // Attest specific credentials
+    /*
+    uport.attestCredentials({
+      sub: THE_RECEIVING_UPORT_ADDRESS,
+      claim: {
+        CREDENTIAL_NAME: CREDENTIAL_VALUE
+      },
+      exp: new Date().getTime() + 30 * 24 * 60 * 60 * 1000, // 30 days from now
+    })
+*/
 
     return cred;
   }
