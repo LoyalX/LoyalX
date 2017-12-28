bdc8a514d335cc40c91c7693c9505340f53dc0d9
Marc Wafik <marc.wafik@hotmail.com>
2017-12-28 15:01:47 +0200

    added transactions page

---
+++ retailer-app/src/pages/transactions/transactions.ts
@@ -0,0 +1,44 @@
+import { Component } from '@angular/core';
+import { IonicPage, NavController, NavParams } from 'ionic-angular';
+
+/**
+ * Generated class for the TransactionsPage page.
+ *
+ * See https://ionicframework.com/docs/components/#navigation for more info on
+ * Ionic pages and navigation.
+ */
+
+@IonicPage()
+@Component({
+  selector: 'page-transactions',
+  templateUrl: 'transactions.html',
+})
+export class TransactionsPage {
+
+  transactions = [
+    {
+      from: "0x627306090abaB3A6e1400e9345bC60c78a8BEf57",
+      to: "0x627306090abaB3A6e1400e9345bC60c78a8BEf57",
+      amount: "500",
+      time: new Date()
+    }, {
+      from: "0x627306090abaB3A6e1400e9345bC60c78a8BEf57",
+      to: "0x627306090abaB3A6e1400e9345bC60c78a8BEf57",
+      amount: "500",
+      time: new Date()
+    }, {
+      from: "0x627306090abaB3A6e1400e9345bC60c78a8BEf57",
+      to: "0x627306090abaB3A6e1400e9345bC60c78a8BEf57",
+      amount: "500",
+      time: new Date()
+    }
+  ];
+
+  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  }
+
+  ionViewDidLoad() {
+    console.log('ionViewDidLoad TransactionsPage');
+  }
+
+}
